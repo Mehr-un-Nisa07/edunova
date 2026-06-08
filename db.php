6e8d0db076d3c1a766256cf8fdd1a4b7574a1715
Mehr-un-Nisa07 <mehro200717@gmail.com>
2026-06-09 03:34:21 +0500

    fix getenv variable names

--- db.php
+++ db.php
@@ -1,17 +1,14 @@
 <?php
-// db.php — Database connection
-$host = getenv('mysql.railway.internal');
-$dbname = getenv('railway');
-$username = getenv('root');
-$password = getenv('fKcsRUAldpWPIKzXrlxajoDMFyvZJmba');
-$port = getenv('3306');
+$host = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
-
-
